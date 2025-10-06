0ef8c6744495007b3d46876d7bd785895990c5d0
Devi <45352250+DeviMaya23@users.noreply.github.com>
2025-10-06 23:49:30 +1100

    feat: add heatmap

--- visualisation.py
+++ visualisation.py
@@ -60,3 +60,22 @@ plt.savefig("result/bat_landing_number_boxplot.png")
 plt.show()
 plt.close()
 
+
+corr = df.corr(numeric_only=True)
+
+# plot the pairwise correlation as heatmap
+ax = sns.heatmap(
+    corr, 
+    vmin=-1, vmax=1, center=0,
+    cmap=sns.diverging_palette(20, 220, n=200),
+    square=True
+)
+
+# customise the labels
+ax.set_xticklabels(
+    ax.get_xticklabels(),
+    rotation=45,
+    horizontalalignment='right'
+)
+
+plt.show()
