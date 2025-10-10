18edd675a43444d2864ca8583205beeacc5c7fa0
asha-devi-hub <ashadhos88@gmail.com>
2025-10-10 14:38:00 +0530

    Create data_init.py

---
+++ spring/data_init.py
@@ -0,0 +1,25 @@
+# Dataset Cleaning Functions for Project Analysis
+
+import pandas as pd
+
+
+def get_cleaned_dataset1():
+    data = pd.read_csv("Datasets/dataset1.csv")
+
+    # delete duplicate rows
+    data = data.drop_duplicates()
+
+    # replace NaN values in habit column to fast
+    data['habit'] = data['habit'].fillna('fast')
+
+    # replace all values with digits in habit column to fast
+    data.loc[data['habit'].str.contains(r'\d', na=False), 'habit'] = 'fast'
+    return data
+
+
+def get_cleaned_dataset2():
+    data = pd.read_csv("Datasets/dataset2.csv")
+
+    # delete duplicate rows
+    data = data.drop_duplicates()
+    return data
